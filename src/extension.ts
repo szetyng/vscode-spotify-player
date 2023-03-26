8ade9143945fc1985e309244de1e7a4b7ab20f15
Sze Tyng <lee.szetyng@gmail.com>
2023-03-26 18:43:27 +0800

    moved player to a class

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,8 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+import { MiniPlayer } from './miniPlayer';
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -10,55 +12,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "spotifyplayer" is now active!');
 
-
-	// register a command that is invoked when the status bar
-    // item is clicked.
-	const currSongClick = 'spotifyplayer.currSongClick';
-	const prevSongClick = 'spotifyplayer.prevSongClick';
-	const pauseSongClick = 'spotifyplayer.pauseSongClick';
-	const nextSongClick = 'spotifyplayer.nextSongClick';
-
-    context.subscriptions.push(vscode.commands.registerCommand(currSongClick, async () => {
-        console.log('clickety click');
-    }));
-	context.subscriptions.push(vscode.commands.registerCommand(prevSongClick, async () => {
-        console.log('clickety click prev');
-    }));
-	context.subscriptions.push(vscode.commands.registerCommand(pauseSongClick, async () => {
-        console.log('clickety click');
-    }));
-	context.subscriptions.push(vscode.commands.registerCommand(nextSongClick, async () => {
-        console.log('clickety click');
-    }));
-	
-	const currSongItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100000);
-	currSongItem.text = `<Current Song> - <Current Artist>`;
-	currSongItem.tooltip = "Hello Spotify!";
-	currSongItem.command = currSongClick;
-
-	const prevSongItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 50000);
-	prevSongItem.text = `prev`;
-	prevSongItem.tooltip = "Hello Spotify prev!";
-	prevSongItem.command = prevSongClick;
-
-	const pauseSongItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 40000);
-	pauseSongItem.text = `pause`;
-	pauseSongItem.tooltip = "Hello Spotify pause!";
-	pauseSongItem.command = pauseSongClick;
-
-	const nextSongItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 40000);
-	nextSongItem.text = `next`;
-	nextSongItem.tooltip = "Hello Spotify next!";
-	nextSongItem.command = nextSongClick;
-
-	context.subscriptions.push(currSongItem);
-	currSongItem.show();
-	context.subscriptions.push(prevSongItem);
-	prevSongItem.show();
-	context.subscriptions.push(pauseSongItem);
-	pauseSongItem.show();
-	context.subscriptions.push(nextSongItem);
-	nextSongItem.show();
+	let miniPlayer: MiniPlayer = new MiniPlayer(context);
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
--- src/miniPlayer.ts
+++ src/miniPlayer.ts
@@ -3,5 +3,68 @@ import * as vscode from "vscode";
 export class MiniPlayer{
     constructor(private context: vscode.ExtensionContext) {
         
+    // register a command that is invoked when the status bar item is clicked.
+	const currSongClick = 'spotifyplayer.currSongClick';
+	const prevSongClick = 'spotifyplayer.prevSongClick';
+	const pauseSongClick = 'spotifyplayer.pauseSongClick';
+	const nextSongClick = 'spotifyplayer.nextSongClick';
+
+    context.subscriptions.push(vscode.commands.registerCommand(currSongClick, async () => {
+        console.log('clickety click');
+    }));
+	context.subscriptions.push(vscode.commands.registerCommand(prevSongClick, async () => {
+        this.prevSong();
+    }));
+	context.subscriptions.push(vscode.commands.registerCommand(pauseSongClick, async () => {
+        this.pauseSong();
+    }));
+	context.subscriptions.push(vscode.commands.registerCommand(nextSongClick, async () => {
+        this.nextSong();
+    }));
+	
+	const currSongItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100000);
+	currSongItem.text = `<Current Song> - <Current Artist>`;
+	currSongItem.tooltip = "Hello Spotify!";
+	currSongItem.command = currSongClick;
+
+	// TODO: icons https://code.visualstudio.com/api/references/icons-in-labels 
+	const prevSongItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 50000);
+	prevSongItem.text = `prev`;
+	prevSongItem.tooltip = "Hello Spotify prev!";
+	prevSongItem.command = prevSongClick;
+
+	const pauseSongItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 40000);
+	pauseSongItem.text = `pause`;
+	pauseSongItem.tooltip = "Hello Spotify pause!";
+	pauseSongItem.command = pauseSongClick;
+
+	const nextSongItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 40000);
+	nextSongItem.text = `next`;
+	nextSongItem.tooltip = "Hello Spotify next!";
+	nextSongItem.command = nextSongClick;
+
+	context.subscriptions.push(currSongItem);
+	currSongItem.show();
+
+	context.subscriptions.push(prevSongItem);
+	prevSongItem.show();
+
+	context.subscriptions.push(pauseSongItem);
+	pauseSongItem.show();
+    
+	context.subscriptions.push(nextSongItem);
+	nextSongItem.show();
+    }
+
+    private pauseSong() {
+        console.log('song paused');
+    }
+
+    private nextSong() {
+        console.log('next song');
+    }
+
+    private prevSong() {
+        console.log('prev song');
     }
 }
